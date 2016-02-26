f37a5d91e53f1e62c41e4f96d4a7214454325be2
Jinha Kim <jinha.kim@oracle.com>
2016-02-25 21:59:33 -0800

    UI ../..

--- personal/theme-goldbar.el
+++ personal/theme-goldbar.el
@@ -7,19 +7,21 @@
 ;; theme
 (add-to-list 'custom-theme-load-path (concat prelude-dir "themes"))
 
-(prelude-require-packages '(moe-theme))
+(prelude-require-packages '(moe-theme solarized-theme))
 (require 'moe-theme)
 ;;(load-theme 'moe-dark t)
 ;;(moe-theme-set-color 'magneta)
 
 (if (window-system)
-    (load-theme 'base16-flat-dark)
+    (load-theme 'solarized-light)
   (load-theme 'moe-dark t)
   )
+(setq solarized-high-contrast-mode-line nil)
+(setq ns-use-srgb-colorspace nil)
 
 ;; powerline
-(setq moe-theme-mode-line-color 'blue)
-(powerline-moe-theme)
+;; (setq moe-theme-mode-line-color 'yellow)
+;; (powerline-moe-theme)
 
 (prelude-require-packages '(spaceline eyebrowse persp-mode window-numbering anzu))
 
@@ -31,14 +33,20 @@
 
 (powerline-reset)
 (require 'spaceline-config)
-(setq powerline-default-separator 'wave)
+(setq powerline-default-separator 'box)
+(setq powerline-height 18)
 (eyebrowse-mode 1)
 (window-numbering-mode 1)
 (setq spaceline-workspace-numbers-unicode t)
 (setq spaceline-window-numbers-unicode t)
 (setq spaceline-highlight-face-func 'spaceline-highlight-face-modified)
 (setq spaceline-minor-modes-p nil)
+(setq anzu-cons-mode-line-p nil)
 (spaceline-spacemacs-theme)
+
+;; major mode icons
+(prelude-require-package 'mode-icons)
+(mode-icons-mode)
 
 (provide 'theme-goldbar)
 ;;; theme-goldbar.el ends here
