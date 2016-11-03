5acaf02235651371cf934e1cc3daf557fba9c2ab
Jinha Kim <jinha.kim@oracle.com>
2016-11-03 12:58:31 -0700

    add ensime for java

--- personal/java-goldbar.el
+++ personal/java-goldbar.el
@@ -1,3 +1,7 @@
+(prelude-require-packages '(ensime))
+
+(require 'ensime)
+(add-hook 'java-mode-hook 'ensime-mode)
 (add-hook 'java-mode-hook (lambda() (set c-basic-offset 2)))
 
 (provide 'java-goldbar)
