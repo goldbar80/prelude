2a43fbf73f7f3edba794cae5199b48c1ffb0fd63
Jinha Kim <jinha.kim@oracle.com>
2016-11-03 15:31:42 -0700

    typo

--- personal/java-goldbar.el
+++ personal/java-goldbar.el
@@ -2,6 +2,6 @@
 
 (require 'ensime)
 (add-hook 'java-mode-hook 'ensime-mode)
-(add-hook 'java-mode-hook (lambda() (set c-basic-offset 2)))
+(add-hook 'java-mode-hook (lambda() (setq c-basic-offset 2)))
 
 (provide 'java-goldbar)
