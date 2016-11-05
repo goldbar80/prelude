00a62591374e5e6ee25e70032934f52e259aff06
Jinha Kim <jinha.kim@oracle.com>
2016-11-05 02:04:39 -0700

    add sbt(mode) conf

--- personal/theme-goldbar.el
+++ personal/theme-goldbar.el
@@ -22,6 +22,8 @@
 
 (require 'all-the-icons)
 (add-to-list 'all-the-icons-icon-alist '("\\.sc$" all-the-icons-alltheicon "scala" :face all-the-icons-red))
+(add-to-list 'all-the-icons-icon-alist '("\\.sbt$" all-the-icons-fileicon "sbt" :face all-the-icons-blue))
+(add-to-list 'all-the-icons-mode-icon-alist '(sbt-mode  all-the-icons-fileicon "sbt" :v-adjust -0.1))
 
 (setq yahoo-weather-location "San Jose, USA")
 (yahoo-weather-mode)
