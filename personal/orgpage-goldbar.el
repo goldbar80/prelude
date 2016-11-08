ae8d14ff98021c49aa96e3f8a35fcb0e0719fd59
Jinha Kim <jinha.kim@oracle.com>
2016-11-08 00:53:30 -0800

    orgpage update

--- personal/orgpage-goldbar.el
+++ personal/orgpage-goldbar.el
@@ -3,12 +3,13 @@
 
 ;; for configuration, refer to 'http://codyreichert.github.io/posts/blogging-with-emacs-and-org-mode/'
 
+(setq op/highlight-render 'htmlize)
 (setq op/repository-directory "~/git/goldbar80.github.io")
 (setq op/site-domain "http://goldbar80.github.io")
 (setq op/personal-github-link "https://github.com/goldbar80")
 (setq op/site-main-title "Meh....")
 (setq op/site-sub-title "Emacs, Programming, and ....")
-(setq op/personal-disqus-shortname "jinha_kim")
-(setq op/theme 'wy)
+(setq op/personal-disqus-shortname "goldbar80github")
+(setq op/theme 'kactus)
 
 (provide 'orgpage-goldbar)
