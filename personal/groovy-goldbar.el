58749ca6eb60027b03d4d0ce6a3e2d2d1985623a
Jinha Kim <jinha.kim@oracle.com>
2016-11-08 17:10:07 -0800

    add indent size

--- personal/groovy-goldbar.el
+++ personal/groovy-goldbar.el
@@ -2,6 +2,7 @@
 
 (prelude-require-package 'groovy-mode)
 (setq groovy-home "/usr/local")
-                                        ;
+(add-hook 'groovy-mode-hook (lambda() (setq c-basic-offset 2)))
+
 (provide 'groovy-goldbar)
 ;;; cpp-goldbar.el ends here
